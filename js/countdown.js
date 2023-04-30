57010c43247faf731cecea66626dc97495739a93
Osci5Dev <97631669+Osci5Dev@users.noreply.github.com>
2023-04-30 13:41:30 +0200

    Add files via upload

---
+++ js/countdown.js
@@ -0,0 +1,32 @@
+const countdown = () => {
+  const countDate = new Date("May 19, 2023 19:15:00").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  const textDays = Math.floor(gap / days);
+  const textHours = Math.floor((gap % days) / hours);
+  const textMinutes = Math.floor((gap % hours) / minutes);
+  const textSeconds = Math.floor((gap % minutes) / seconds);
+
+  const countdownElement = document.getElementById("countdown");
+  if (gap > 0) {
+    countdownElement.innerHTML = `
+      <div>${textDays}<span>Dni</span></div>
+      <div>${textHours}<span>Godziny</span></div>
+      <div>${textMinutes}<span>Minuty</span></div>
+      <div>${textSeconds}<span>Sekundy</span></div>
+    `;
+  } else {
+    clearInterval(intervalId);
+    // Co sie gapisz, to i tak jest placeholder.
+    // Poczekaj na koniec licznika to się dowiesz co się stanie 19 Maja 
+    countdownElement.innerHTML = "<h1>Wlasnie minelo 1000 dni daily</h1>";
+  }
+};
+
+const intervalId = setInterval(countdown, 1000);
---
+++ js/countdown_jazda.js
@@ -0,0 +1,32 @@
+const countdown_jazda = () => {
+  const countDate = new Date("December 31, 2023 23:59:59").getTime();
+  const now = new Date().getTime();
+  const gap = countDate - now;
+
+  const seconds = 1000;
+  const minutes = seconds * 60;
+  const hours = minutes * 60;
+  const days = hours * 24;
+
+  const textDays = Math.floor(gap / days);
+  const textHours = Math.floor((gap % days) / hours);
+  const textMinutes = Math.floor((gap % hours) / minutes);
+  const textSeconds = Math.floor((gap % minutes) / seconds);
+
+  const countdownElement = document.getElementById("countdown_jazda");
+  if (gap > 0) {
+    countdownElement.innerHTML = `
+      <div>${textDays}<span>Dni</span></div>
+      <div>${textHours}<span>Godziny</span></div>
+      <div>${textMinutes}<span>Minuty</span></div>
+      <div>${textSeconds}<span>Sekundy</span></div>
+    `;
+  } else {
+    clearInterval(intervalId_jazda);
+    // Co sie gapisz, to i tak jest placeholder.
+    // Poczekaj na koniec licznika to się dowiesz co się stanie 19 Maja 
+    countdownElement.innerHTML = "<h1>Wlasnie minelo 1000 dni daily</h1>";
+  }
+};
+
+const intervalId_jazda = setInterval(countdown_jazda, 1000);
